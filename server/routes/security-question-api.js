e3fb311161c72fac90de78be7818808ba137e81c
Jonathan Roland <jdroland31@yahoo.com>
2021-04-17 19:39:41 -0400

    add UpdateSecurityQuestions to security questions API

--- server/routes/security-question-api.js
+++ server/routes/security-question-api.js
@@ -23,11 +23,39 @@ const ErrorResponse = require('../services/error-response');
 const router = express.Router();
 
 /**
- **
- ** Find All SQ
- **
- **/
+ * API: FindAll
+ * @returns All Security Questions documents or null
+ * This route returns all security questions from the database
+*/
 
+ router.get('/', async (req, res) => {
+  try {
+    // Attempt to query for all security questions.
+    SecurityQuestion.find({}, function(err, securityQuestions){
+
+      //If the database encounters an error, log the error to the console and output it as an object.
+      if (err) {
+        console.log(err);
+        const findAllSecurityQuestionsMongodbErrorResponse = new ErrorResponse(500, 'Internal server error', err);
+        res.status(500).send(findAllSecurityQuestionsMongodbErrorResponse.toObject());
+      }
+
+      //If successful, return the user object that was found.
+      else {
+        console.log(securityQuestions);
+        const findAllSecurityQuestionsResponse = new BaseResponse(200, 'Query successful!', securityQuestions);
+        res.json(findAllSecurityQuestionsResponse.toObject());
+      }
+    });
+  }
+  // If there is an error with the server, return a 500 code and the error.
+  catch (e) {
+    console.log(e);
+    const findAllSecurityQuestionsCatchErrorResponse = new ErrorResponse (500, 'Internal Server Error', e);
+    res.status(500).send(findAllSecurityQuestionsCatchErrorResponse.toObject());
+  }
+
+});
 
 
 /**
@@ -75,12 +103,70 @@ const router = express.Router();
 });
 
 
- /**
- **
- ** Update SQ
- **
- **/
+/**
+ * API: UpdateSecurityQuestion
+ * @returns
+ * @params
+*/
 
+router.put('/:_id', async(req,res) => {
+  try
+  {
+    SecurityQuestion.findOne({'_id': req.params._id}, function (err, securityQuestion)
+    {
+      if(err)
+      {
+        //If the database encounters an error, log the error to console and output as an object.
+        console.log(err);
+        const updateSecurityQuestionMongoDBException = new BaseResponse('500',`Internal Server Error ${err.message}`,null);
+        res.status(500).send(updateSecurityQuestionMongoDBException.toObject());
+      }
+      else
+      {
+        //If no error and the security question is valid, proceed with updating the security question.
+        console.log(securityQuestion);
+        if(securityQuestion)
+        {
+          //Set the security question's text attribute to match that in the request body.
+          securityQuestion.set({
+            text: req.body.text
+          });
+          //Save the new security question data.
+          securityQuestion.save(function(err, updatedSecurityQuestion){
+            if(err)
+            {
+              //If the database encounters an error while attempting to update, log the error to console and output as an object.
+              console.log(err);
+              const updatedSecurityQuestionMongoDBError = new BaseResponse('500',`Internal Server Error ${err.message}`,null);
+              res.status(500).send(updatedSecurityQuestionMongoDBError.toObject());
+            }
+            else
+            {
+              //If successful, log and return the updated security question document.
+              console.log(updatedSecurityQuestion);
+              const updatedSecurityQuestionSuccessResponse = new BaseResponse('200', 'Query Successful', updatedSecurityQuestion);
+              res.status(200).send(updatedSecurityQuestionSuccessResponse.toObject());
+            }
+          })
+        }
+        else
+        {
+          //if the security question Id is invalid, log and return null along with error message. 200 code is returned since the request technically succeeded, but lacked a valid user.
+          console.log(`Invalid security question id! The passed-in value was ${req.params._id}`);
+          const invalidSecurityQuestionIdResponse = new BaseResponse('200','Invalid security question id', securityQuestion);
+          res.status(200).send(invalidSecurityQuestionIdResponse.toObject());
+        }
+      }
+    })
+  }
+  catch (e)
+  {
+    //If the server encounters an error event, catch it, log it and return the error as an object.
+    console.log(e);
+    const updateSecurityQuestionCatchResponse = new BaseResponse('500',`Internal Server Error ${err.message}`,null);
+    res.status(500).send(updateSecurityQuestionCatchResponse.toObject());
+  }
+})
 
  /**
  **
