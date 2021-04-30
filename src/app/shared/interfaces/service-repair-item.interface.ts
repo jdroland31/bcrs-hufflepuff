2153d8b07a227e614d67ecb80cc398fbb5c2baa2
Wendy Leon <pixelmamma@gmail.com>
2021-04-30 19:54:41 -0400

    Adding home page code

---
+++ src/app/shared/interfaces/service-repair-item.interface.ts
@@ -0,0 +1,13 @@
+/***
+** Title: Bob's Computer Repair Shop
+** Author:  Professor Krasso
+** Modified by: Jonathan Roland, Nicole Barleta, Wendy Leon
+** Date: April 30 2021
+** Description: service repair service
+ ***/
+//This interface defines a service repair item object.
+export interface ServiceRepairItem {
+  id: string;
+  title: string;
+  price: number;
+}
